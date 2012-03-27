7783a46220d179850c2cb15422f5710a05d41a25
Tássio Auad <tassioauad@id.uff.br>
2012-03-27 16:15:33 -0300

    Troca de telas utilizando uma instância da classe Intent. Não foi bem sucessido, pois a troca não é realizada, indicando um erro.

--- src/android/pack/IMC/IMCActivity.java
+++ src/android/pack/IMC/IMCActivity.java
@@ -2,6 +2,7 @@ package android.pack.IMC;
 
 import android.pack.IMC.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +16,12 @@ public class IMCActivity extends Activity {
         setContentView(R.layout.main); //Indica que o main é o xml com o visual.
         
         final Button button = (Button) findViewById(R.id.buttonCalcular);
+		final Intent troca = new Intent();
+        troca.setClass(this, Resultado.class);
 		
 		button.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				setContentView(R.layout.resultado);
+				startActivity(troca);
 			}
 		});
     }
