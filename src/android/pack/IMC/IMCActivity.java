c3189d2290575b947932243f8802c232832f9382
Tássio Auad <tassioauad@id.uff.br>
2012-03-28 00:24:50 -0300

    Corrigido os erros. O app está funcionando corretamente, exibindo os valores Altura, Peso e o IMC relativo. Falta adicionar o Status do IMC e algum tipo de conselhor. Mas pretendo fazer essa troca de valores entre as telas por meio de parametros e não por meio do uso de campos statics.

--- src/android/pack/IMC/IMCActivity.java
+++ src/android/pack/IMC/IMCActivity.java
@@ -13,19 +13,19 @@ public class IMCActivity extends Activity {
 
 	static String pesoValue;
 	static String alturaValue;
-	final EditText Peso = (EditText) findViewById(R.id.editTextPeso);
-    final EditText Altura = (EditText) findViewById(R.id.editTextAltura);
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main); //Indica que o main é o xml com o visual.
-        
+
     }
     
     public void clique(View v)
     {
     	final Intent i = new Intent(); //Intanciando uma Intent para troca de telas.
     	i.setClass(this, Resultado.class); // Informando qual será a troca de telas.
+    	final EditText Peso = (EditText) findViewById(R.id.editTextPeso);
+        final EditText Altura = (EditText) findViewById(R.id.editTextAltura);
     	pesoValue = Peso.getText().toString();
     	alturaValue = Altura.getText().toString();
     	startActivity(i); // Ativando a Intenet i
--- src/android/pack/IMC/Resultado.java
+++ src/android/pack/IMC/Resultado.java
@@ -6,20 +6,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Resultado extends Activity {
-	
-	final TextView ResultadoAltura = (TextView) findViewById(R.id.textViewResultAltura);
-	final TextView ResultadoPeso = (TextView) findViewById(R.id.textViewResultPeso);
-	final TextView ResultadoIMC = (TextView) findViewById(R.id.textViewResultIMC);
-	final TextView ResultadoStatus = (TextView) findViewById(R.id.textViewResultStatus);
-	final TextView ResultadoSugestao = (TextView) findViewById(R.id.textViewResultConselho);
-	private Float imcValue;
-	
+		
 	 public void onCreate(Bundle Resultado) {
 	        super.onCreate(Resultado);
 	        setContentView(R.layout.resultado); //Indica que o main é o xml com o visual.
 
-	        imcValue = Float.parseFloat(IMCActivity.pesoValue)/(Float.parseFloat(IMCActivity.alturaValue) * Float.parseFloat(IMCActivity.alturaValue));
-			
+	    	final TextView ResultadoAltura = (TextView) findViewById(R.id.textViewResultAltura);
+	    	final TextView ResultadoPeso = (TextView) findViewById(R.id.textViewResultPeso);
+	    	final TextView ResultadoIMC = (TextView) findViewById(R.id.textViewResultIMC);
+	    	final TextView ResultadoStatus = (TextView) findViewById(R.id.textViewResultStatus);
+	    	final TextView ResultadoSugestao = (TextView) findViewById(R.id.textViewResultConselho);
+	    	Float imcValue;
+
+	        
+	        imcValue = Float.parseFloat(IMCActivity.pesoValue)/(Float.parseFloat(IMCActivity.alturaValue) * Float.parseFloat(IMCActivity.alturaValue));			
 			ResultadoAltura.setText("Altura: "+ IMCActivity.alturaValue);
 			ResultadoPeso.setText("Peso: " + IMCActivity.pesoValue);
 			ResultadoIMC.setText("IMC: " + imcValue);
