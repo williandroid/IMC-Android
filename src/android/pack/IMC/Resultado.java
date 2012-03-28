a1556a74becd5eeddb0531456ff1fa14c4870e5c
Tássio Auad <tassioauad@id.uff.br>
2012-03-28 20:19:32 -0300

    Botão de novo cálculo na segunda tela está funcionando.

--- src/android/pack/IMC/Resultado.java
+++ src/android/pack/IMC/Resultado.java
@@ -3,17 +3,21 @@ package android.pack.IMC;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.pack.IMC.IMCActivity;
 
 public class Resultado extends Activity {
 	 
 	 Float imcValue;
-	
+	 final Intent a = new Intent();
+	 
 	 public void onCreate(Bundle Resultado) {
 	        super.onCreate(Resultado);
 	        setContentView(R.layout.resultado); //Indica que o main é o xml com o visual.
-
+	        a.setClass(this, IMCActivity.class);
+	        
 	    	final TextView ResultadoAltura = (TextView) findViewById(R.id.textViewResultAltura);
 	    	final TextView ResultadoPeso = (TextView) findViewById(R.id.textViewResultPeso);
 	    	final TextView ResultadoIMC = (TextView) findViewById(R.id.textViewResultIMC);
@@ -31,6 +35,11 @@ public class Resultado extends Activity {
 			ResultadoStatus.setText("Status: ");
 	       
 			
+	 }
+	 
+	 public void voltar(View v)
+	 {
+		 startActivity(a);
 	 }
 	 
 }
