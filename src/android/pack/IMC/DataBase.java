87d635fb7c2c4d6cc1abe89264cc58d460218ddf
Tássio Auad <tassioauad@id.uff.br>
2012-04-03 12:13:55 -0300

    Inseri o método de buscar dados no database usando o elemento autor como filtro. Adicionei o campos calculos do tipo ArrayList<Calculo> na classe DataBase para armazenar todos os resultado da busca e como consequencia adicionei seus métodos set e get.

--- src/android/pack/IMC/DataBase.java
+++ src/android/pack/IMC/DataBase.java
@@ -1,13 +1,16 @@
 package android.pack.IMC;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DataBase {
 
-	private static final String NOME_BANCO = "IMC";
-	private static final String NOME_TABELA = "calculos";
+	private final String NOME_BANCO = "IMC";
+	private final String NOME_TABELA = "calculos";
+	private ArrayList<Calculo> calculos = new ArrayList<Calculo>();
 	private final SQLiteDatabase db;
 	
 	public DataBase(Context ctx)
@@ -15,13 +18,35 @@ public class DataBase {
 		db = ctx.openOrCreateDatabase(NOME_BANCO, Context.MODE_PRIVATE, null);
 	}
 	
-	public String buscar(String autor)
+	public void buscar(String autor)
 	{
 		Cursor resposta = db.query(NOME_TABELA, new String[] {"_id", "autor", "data", "peso", "altura", "imc"}, "autor=?", new String[]{autor}, null, null, null);
 		if(resposta.getCount() > 0)
 		{
+			int count = 1;
+			resposta.moveToFirst();
+			while(count < resposta.getCount())
+			{
+				Calculo calculo = new Calculo();
+				calculo.setAutor(resposta.getString(2));
+				calculo.setData_insercao(resposta.getString(3));
+				calculo.setPeso(resposta.getFloat(4));
+				calculo.setAltura(resposta.getFloat(5));
+				calculo.setImc(resposta.getFloat(6));
+				calculos.add(calculo);
+				resposta.moveToNext();
+				count ++;
+			}
 			
 		}
-		return "";
-	}	
+	}
+	
+	public ArrayList<Calculo> getCalculos() {
+		
+		return calculos;
+	}
+
+	public void setCalculos(ArrayList<Calculo> calculos) {
+		this.calculos = calculos;
+	}
 }
