5b4a52c36cb3501429ab8db0814be3ec64beed4f
Tássio Auad <tassioauad@id.uff.br>
2012-04-03 14:07:55 -0300

    Criada a classe menu que conterá um ListActivity com as opções. Ainda faltam alguns complementos na classe.

---
+++ src/android/pack/IMC/Menu.java
@@ -0,0 +1,34 @@
+package android.pack.IMC;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Menu  extends ListActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		final String[] itens = new String[] {"Novo Cálculo", "Histórico", "Sair"};
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itens);
+		setListAdapter(adapter);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Object o = this.getListAdapter().getItem(position);
+		String item = o.toString();
+		if(item == "0")
+		{
+			final Intent i = new Intent();
+			i.setClass(this, IMCActivity.class);
+			startActivity(i); 
+		}
+	}
+	
+	
+}
